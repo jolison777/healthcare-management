96de7463b756e3f9566e197205a4ae5e50265542
jolison777 <jolisonraj7@gmail.com>
2026-09-14 18:38:58 +0530

    Add backend context test

---
+++ backend/src/test/java/com/medicare360/MediCare360ApplicationTests.java
@@ -0,0 +1,3 @@
+package com.medicare360;
+import org.junit.jupiter.api.Test; import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest class MediCare360ApplicationTests { @Test void contextLoads(){} }
